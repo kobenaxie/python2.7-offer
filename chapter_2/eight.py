1ed7f0ee58e1be3422a6271d3ee484a77b3d7056
kobenaxie <kobena2014@163.com>
2018-07-11 15:37:01 +0800

    旋转数组的最小数字【及索引】
    把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素及其位置索引。例如数组【3,4,5,1,2】是数组的【1,2,3,4,5】的一个旋转，该数组的最小值为1，首个位置索引值【因为有个可能存在重复最小值】

---
+++ chapter_2/eight.py
@@ -0,0 +1,35 @@
+# coding:utf-8
+"""
+二分法：分成两个排序的数组，两个指针分别指向两个排序数组，两个数组的交界处分别为数组最大值及最小值。
+修改查找旋转数组的最小值，增加了最小值索引位置的需求。
+"""
+def min_rotate_array(rotate_array):
+    idx_left = 0
+    idx_right = len(rotate_array)-1
+    idx_mid = 0
+    while rotate_array[idx_left] >= rotate_array[idx_right]:
+        if idx_right-idx_left == 1:
+            return rotate_array[idx_right], idx_right
+
+        idx_mid = (idx_right + idx_left)/2
+
+        if rotate_array[idx_mid] == rotate_array[idx_left] == rotate_array[idx_right]:
+            min, min_idx = get_min(rotate_array[idx_left:idx_right])
+            return min, min_idx+idx_left
+
+        if rotate_array[idx_mid] >= rotate_array[idx_left]:
+            idx_left = idx_mid
+        elif rotate_array[idx_mid] <= rotate_array[idx_right]:
+            idx_right = idx_mid
+
+    return rotate_array[idx_mid], idx_mid
+
+
+def get_min(array):
+    min = array[0]
+    idx = 0
+    for i, elem in enumerate(array):
+        if min >= elem:
+            min = elem
+            idx = i
+    return min, idx
